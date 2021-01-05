84fb04a84a5fcb051c7a75d6a5294eb5c889ed68
linda paolieri <me@lindapaolieri.com>
2021-01-05 17:13:22 +0100

    add wrong test to user service

--- services/userService_test.go
+++ services/userService_test.go
@@ -8,12 +8,8 @@ import (
 	"testing"
 )
 
-func TestSave(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
-	mock := mocks.NewMockIUserRepository(mockCtrl)
-
+func TestCheckPasswordOk(t *testing.T) {
+	mock := mockUserRepository(t)
 	hashedPassword, _ := utils.HashPassword("password")
 	mock.
 		EXPECT().
@@ -24,4 +20,23 @@ func TestSave(t *testing.T) {
 	if err := SUT.CheckUserPassword("mail", "password"); err != nil {
 		t.Error("Check Hash Password Failed")
 	}
+}
+
+func TestCheckPasswordWrong(t *testing.T) {
+	mock := mockUserRepository(t)
+	mock.
+		EXPECT().
+		SearchByMail("mail").
+		Return(models.NewUser("name", "surname", "notOkPassword", "mail"))
+
+	SUT := UserService{mock}
+	if err := SUT.CheckUserPassword("mail", "password"); err == nil {
+		t.Error("Check wrong password failed")
+	}
+}
+
+func mockUserRepository(t *testing.T) *mocks.MockIUserRepository {
+	mockCtrl := gomock.NewController(t)
+	mock := mocks.NewMockIUserRepository(mockCtrl)
+	return mock
 }
